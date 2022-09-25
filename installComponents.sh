ddfee8cad05d57e23343e139d8bf7e83e6496f7b
kameshX007 <kameshbook@gmail.com>
2022-09-25 13:17:32 +0530

    Refining the code

--- installComponents.sh
+++ installComponents.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #Installing Docker
 apt -y update;
-apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose docker-compose-plugin
+apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose docker-compose-plugin;
 exit 0
--- installDocker.sh
+++ installDocker.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
+getUserName(){
+    echo "Please enter the username";
+    echo "We will create a docker folder inside this users home directory to store the docker persistant volume"
+
+    read x;
+    if [ -z $x ]
+    then
+        echo "No Username entered, defaulting to ubuntu" && dockerUser = "ubuntu";
+    else
+        dockerUser = $x;
+    fi
+}
 #Installing prerequisite
 echo "Installing prerequisite...">installDocker.log;
 sudo sh installPrerequisite.sh;
@@ -7,4 +19,13 @@ sudo sh installPrerequisite.sh;
 echo "Installing Docker...">installDocker.log;
 sudo sh installComponents.sh;
 echo "Docker installed successfully...">>installDocker.log;
+
+#Adding docker to the user group
+echo "Adding docker to the user group...">installDocker.log;
+getUserName;
+sudo usermod -aG docker $dockerUser;
+#Creating a network for docker on whcoh all containers will get deployed
+echo "Creating a network for docker on whcoh all containers will get deployed...">installDocker.log;
+docker network create tunnel;
+echo "tunnel network created...">installDocker.log;
 exit 0
